95559911f9de0c9f5c7d5901fbd71168ee22debf
rajraku <koolraj@gmail.com>
2015-01-18 18:14:27 +0530

    updated the code to get the templates from mongodb
    updated page action to get the templates from my mongo lab.
    
    Connected with angular app for easy client MVC framework

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
-var angularApp = angular.module('templates', []);
+var templateApp = angular.module('templatesApp', []);
 
-angularApp
-.register
-.controllers('MyDetailsController', ['$rootScope', '$scope', function($rootScope, $scope){
-
-	$scope.onSearch = function(value)
-	{
-
+templateApp
+.controller('TemplatesController', ['$http', '$scope', function($http, $scope){
+	$scope.copyTemplateValue = function(template){
+		if(template){
+			copyTextToClipboard(template.text);
+		}
+		return false;
 	}
+	$http.get(''). //paste the url from where you will get the data from.......
+	  success(function(data, status, headers, config) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+	    $scope.templatesData = data;
+	  }).
+	  error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+}]);
 
-	
-});
+function copyTextToClipboard(text) {
+    var copyFrom = $('<textarea/>');
+    copyFrom.text(text);
+    $('body').append(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy', true);
+    copyFrom.remove();
+
+}
